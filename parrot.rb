a4726363b4ae5168fefc1af68f5ffa110fc665eb
Jessica Gamez <jpgamez1789@icloud.com>
2020-04-19 04:25:24 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,11 @@
-# Create method `parrot` that outputs a given phrase and
-# returns the phrase
+def parrot(phrase = "squawk!")
+  puts phrase
+end
+
+parrot("Hello world")
+parrot()
+parrot()
+parrot()
+parrot()
+parrot()
+parrot("Hi!!")
